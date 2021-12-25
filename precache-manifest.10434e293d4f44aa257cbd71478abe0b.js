baf02a491e8c7f8d0901db31269c889c90ba76ae
balajeeav <guytrendy6@gmail.com>
2021-12-25 17:53:07 +0530

    Updates

---
+++ precache-manifest.10434e293d4f44aa257cbd71478abe0b.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3072cca54e67f17e1870ff9830636d40",
+    "url": "/welcome/index.html"
+  },
+  {
+    "revision": "050407b5d6878ed53725",
+    "url": "/welcome/static/css/main.47a51ebd.chunk.css"
+  },
+  {
+    "revision": "1b670b1c3649a1087d87",
+    "url": "/welcome/static/js/2.f592a2b4.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/welcome/static/js/2.f592a2b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050407b5d6878ed53725",
+    "url": "/welcome/static/js/main.cd3b460c.chunk.js"
+  },
+  {
+    "revision": "d21beac86e1125ce7f1f",
+    "url": "/welcome/static/js/runtime-main.7286a67b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/welcome/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/welcome/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/welcome/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/welcome/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/welcome/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/welcome/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/welcome/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/welcome/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1fdc9e7602e9840e46b8be1f8612ceac",
+    "url": "/welcome/static/media/balajee.1fdc9e76.jpg"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "/welcome/static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "0ae766793c719b062dfe703f928918d9",
+    "url": "/welcome/static/media/cisco.0ae76679.png"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "/welcome/static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/welcome/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/welcome/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/welcome/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/welcome/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/welcome/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/welcome/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/welcome/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/welcome/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/welcome/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/welcome/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/welcome/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/welcome/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/welcome/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/welcome/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/welcome/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "/welcome/static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "6364dbb3a5b59c28566eac7e6aaacd04",
+    "url": "/welcome/static/media/gdg.6364dbb3.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/welcome/static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/welcome/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "f7fc29c208db59b75cbfdcf90bce8d06",
+    "url": "/welcome/static/media/google_cloud.f7fc29c2.jpg"
+  },
+  {
+    "revision": "7a2dbf7a2595600c00118eea8a7a52f8",
+    "url": "/welcome/static/media/hackathon1.7a2dbf7a.svg"
+  },
+  {
+    "revision": "aa3b2e77974feac55d4cee0262121bf5",
+    "url": "/welcome/static/media/ibm.aa3b2e77.jpg"
+  },
+  {
+    "revision": "36d7942a201d6e2b83b55876e8433ccf",
+    "url": "/welcome/static/media/iit.36d7942a.png"
+  },
+  {
+    "revision": "ccf886614a9c68f209d3e95c2626dd99",
+    "url": "/welcome/static/media/ino.ccf88661.png"
+  },
+  {
+    "revision": "cad32a896c9d72d7a5450c7a20434d20",
+    "url": "/welcome/static/media/kln.cad32a89.png"
+  },
+  {
+    "revision": "847873a85df8045d1d51cc6cf9cc97d7",
+    "url": "/welcome/static/media/localguide.847873a8.gif"
+  },
+  {
+    "revision": "b7f903d747b0409347c9ee67745dbfa4",
+    "url": "/welcome/static/media/localguide.b7f903d7.png"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "/welcome/static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "53dd27a8801e3a4216b9c9e217dfc7d0",
+    "url": "/welcome/static/media/mlh.53dd27a8.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "/welcome/static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "5dcd18d03068fbd9edeeb5003b21998a",
+    "url": "/welcome/static/media/nightowls.5dcd18d0.jpg"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "/welcome/static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "/welcome/static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "510563618ea90bd860a11f2429aa67e0",
+    "url": "/welcome/static/media/pwc-logo.51056361.jpg"
+  },
+  {
+    "revision": "7b7a76ef55658f6999ce00c5d55f85a5",
+    "url": "/welcome/static/media/skillenza.7b7a76ef.png"
+  },
+  {
+    "revision": "02fdae4350ca45cabdc9c40cc11efc2a",
+    "url": "/welcome/static/media/wrighter.02fdae43.jpg"
+  }
+]);
