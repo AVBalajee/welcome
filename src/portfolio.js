6acacc63f9464787ff1503383d04bdf3a4a8c939
balajeeav <guytrendy6@gmail.com>
2021-12-25 16:13:20 +0530

    initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,611 @@
+
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hi 👋.",
+  title2: "Balajee",
+  logo_name: "AVB()",
+  nickname: "Tech Klug",
+  full_name: "Balajee A V",
+  subTitle:
+    "Engineer, Open Source Programmer 🔥. Sometimes learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/1KCBZ1b9wH_ARa1fy6TP_CpXavyw7lhG0/view?usp=drivesdk",
+  mail: "mailto:balajeevg@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/AVBalajee",
+  linkedin: "https://www.linkedin.com/in/balajeevg-techclog/",
+  gmail: "balajeevg@gmail.com",
+  twitter:"https://twitter.com/AVBalajee1",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications",
+        "⚡ Building responsive website front end using ReactJS",
+        "⚡ Developing mobile applications using Flutter and Android",
+        "⚡ Creating application backend in Node, Express & Flask",
+        "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        // {
+        //   skillName: "GraphQL",
+        //   fontAwesomeClassname: "simple-icons:graphql",
+        //   style: {
+        //     color: "#DE33A6",
+        //   },
+        // },
+        {
+          skillName: "Android",
+          fontAwesomeClassname: "simple-icons:android",
+          style: {
+            color: "#3DDC84",
+          },
+        },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        {
+          skillName: "Flutter",
+          fontAwesomeClassname: "simple-icons:flutter",
+          style: {
+            color: "#02569B",
+          },
+        },
+        {
+          skillName: "Dart",
+          fontAwesomeClassname: "simple-icons:dart",
+          style: {
+            color: "#29B0EE",
+          },
+        },
+        // {
+        //   skillName: "Visual Basic",
+        //   fontAwesomeClassname: "simple-icons:dot-net",
+        //   style: {
+        //     color: "#029FCE",
+        //   },
+        // },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+        {
+          skillName: "jQuery",
+          fontAwesomeClassname: "simple-icons:jquery",
+          style: {
+            color: "#0865A6",
+          },
+        },
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "K.L.N.College Of Engineering",
+      subtitle: "Bachelor degree in Electronics and Communication",
+      logo_path: "kln.png",
+      alt_name: "KLN",
+      duration: "2019-2023 (Present)",
+      descriptions: [
+        "⚡ I'm currently pursuing my Bachelors in Electronics and Communication .",
+        "⚡ I have studied core subjects like Electronics,DSP,Control systems, etc.",
+        "⚡ I have also completed various online courses for Cybersecurity,Web Design,Arduino Programming etc.",
+        "⚡ I have implemented several projects based on what I've leart under my ECE Engineering course. ",
+      ],
+      website_link: "http://klnce.edu/",
+    },
+    
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Google Professional IT Support",
+      subtitle: "Google",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/certificate/7CLKDQUCYA35",
+      alt_name: "Google",
+       color_code: "#FFFFFF",
+      //color_code: "#47A048",
+    },
+    {
+      title: "Google Cloud Security",
+      subtitle: "Google Cloud",
+      logo_path: "google_cloud.jpg",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/YUFBLENYRBDN",
+      alt_name: "Google Cloud",
+      color_code: "#FFFFFF",
+    },
+    {
+      title: "SAS Advanced Programmer",
+      subtitle: "SAS",
+      logo_path: "sas.png",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/certificate/RPRWRHQ3EJAK",
+      alt_name: "SAS",
+      // color_code: "#F6B808",
+      color_code: "#00CCCC",
+    },
+    {
+      title: "IBM z/OS Mainframe Practioner",
+      subtitle: "IBM",
+      logo_path: "ibm.jpg",
+      certificate_link:
+        "https://www.coursera.org/account/accomplishments/specialization/certificate/6XZ78RUEJZN5",
+      alt_name: "IBM",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "Ethical Hacking",
+      subtitle: "Guvi",
+      logo_path: "guvi.png",
+      certificate_link:
+        "https://www.guvi.in/verify-certificate?id=j21zYN82182Od2626I&course=ethicalHackingWebTa",
+      alt_name: "Guvi",
+      color_code: "#00FF00",
+    },
+    {
+      title: "CISSP",
+      subtitle: "Cybrary",
+      logo_path: "cybrary.png",
+      certificate_link:
+        "https://app.cybrary.it/courses/api/certificate/CC-21f73c20-d820-4941-8b82-dce93a374997/view",
+      alt_name: "Cybrary",
+      color_code: "#FFFFFF",
+    },
+    
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, Internship and Volunteership",
+  description:
+    "I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Cybersecurity Virtual (Intern)",
+          company: "PricewaterhouseCoopers",
+          company_url: "http://www.pwc.com/",
+          logo_path: "pwc-logo.jpg",
+          duration: "May 2021",
+          location: "Remote",
+          description:
+          `Integrated Information Defense,
+           Risk Assessment, 
+           IT System Security Baseline,
+           Network Segmentation
+          `,
+            
+          color: "#0071C5",
+        },
+        {
+          title: "Technical Consulting",
+          company: "SAP",
+          company_url: "http://sap.com/",
+          logo_path: "sap.png",
+          duration: "Jun 2021",
+          location: "Remote",
+          description:
+          `Assemble the Data,
+          Data Analysis,
+          Present the Results
+          `,
+           
+          color: "#ee3c26",
+        }
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        
+        
+        {
+          title: "GitHub Student Developer",
+          company: "GitHub",
+          company_url: "https://github.com/",
+          logo_path: "github.png",
+          duration: "Jan 2020 - Present",
+          location: "Work from Home",
+          description:
+          "Contribute to Open Source Community and Open Source Project.",
+          color: "#040f26",
+        },
+        {
+          title: "Google Local Guide",
+          company: "Google Map",
+          company_url: "https://maps.google.com/localguides/",
+          logo_path: "localguide.png",
+          duration: "Apr 2019 - Present",
+          location: "Work From Home",
+          description:
+            "Day-to-day responsibilities of helping local businesses to spread their business to the world. Helping users by writing reviews about different locations and spaces such as shops, malls, etc.",
+          color: "#D83B01",
+        },
+        {
+          title: "GDG Student Volunteer",
+          company: "Google Developer Groups",
+          company_url: "https://gdg.community.dev/",
+          logo_path: "gdg.png",
+          duration: "Feb 2021 - Present",
+          location: "Work From Home",
+          description:
+            "Google Developer Groups Student Volunteer and Member.",
+          color: "#D83B01",
+        },
+        
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create Python Scripts, and React Project. Below are some of my projects.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "balajee.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog usually but when I do something awesome, I do try to document it so it can be helpful to others. I write on LinkedIn.",
+    link: "https://www.linkedin.com/in/balajeevg-techclog/",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Traffic Sign Classification",
+      url: "https://github.com/AVBalajee/Traffic-sign-classification",
+      description: "This is a python program for traffic sign classification which is used in Self Driving Cars",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Personal Website",
+      url: "https://balajee-av.netlify.app/",
+      description:
+        "My Personal Portfolio :)",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Rainfall Prediction",
+      url: "https://github.com/balajeeav/Rainfall-Prediction",
+      description:
+        "A prediction on Water Drop and Humidity Check using Machine Learning",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        
+      ],
+    },
+    {
+      id: "3",
+      name: "Scoreboard",
+      url: "https://github.com/AVBalajee/Scorecard-reactjs",
+      description:
+        "A simple react-js Scorecard ",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "Covid Cases Prediction",
+      url: "https://github.com/balajeeav/covid-cases-prediction",
+      description:
+        "Prediction on Covid cases using different Machine Learning Algorithms",
+      languages: [
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        
+      ],
+    },
+    
+    {
+      id: "5",
+      name: "Chatbot",
+      url: "https://github.com/balajeeav/Chatbot",
+      description:
+        "A simple pybot for customer service",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        {
+          name: "SCSS",
+          iconifyClass: "vscode-icons:file-type-scss2",
+        },
+      ],
+    },
+    
+    
+    
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
